1e3a60d970ae3a8fc96ba229a06876f3a9705aab
Sowmya <sowmya.manu@yahoo.com>
2022-02-09 22:19:51 +0000

    Implementation of exercise003

--- challenges/exercise004.js
+++ challenges/exercise004.js
@@ -1,43 +1,70 @@
 function findSmallNums(nums) {
+  //Input Validation
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  //Filter the  array numbers less than 1
+  return nums.filter((item) => item < 1);
 }
 
 function findNamesBeginningWith(names, char) {
+  //Input Validation
   if (!names) throw new Error("names is required");
   if (!char) throw new Error("char is required");
-  // Your code here
+
+  //Filtering the given array names with starting character
+  return names.filter(item => item.charAt(0) === char);
 }
 
+
 function findVerbs(words) {
+  //Input Validation
   if (!words) throw new Error("words is required");
-  // Your code here
+  //Filtering the array with starting characters "to "
+  return words.filter(word => word.substring(0, 3) === "to ");
 }
 
 function getIntegers(nums) {
+  //Input Validation
   if (!nums) throw new Error("nums is required");
-  // Your code here
+  // filtering the array with only integers
+  return nums.filter(num => num % 1 === 0);
 }
 
 function getCities(users) {
+  //Input Validation
   if (!users) throw new Error("users is required");
-  // Your code here
+
+  //Getting the city display from users object
+  let cityArr = [];
+  users.forEach(user => cityArr.push(user.data.city.displayName));
+  return cityArr;
 }
 
 function getSquareRoots(nums) {
+  //Input Validation
   if (!nums) throw new Error("nums is required");
-  // Your code here
+
+  // Taking the square root of the array
+  let sqrNum = [];
+  nums.forEach(num => sqrNum.push(Number(Math.sqrt(num).toFixed(2))));
+  return sqrNum;
 }
 
 function findSentencesContaining(sentences, str) {
+  //Input Validation
   if (!sentences) throw new Error("sentences is required");
   if (!str) throw new Error("str is required");
-  // Your code here
+  // Filtering the string from the sentences by converting into lowercase (string) format
+  return sentences.filter(sentence => sentence.toLowerCase().includes(str));
 }
 
 function getLongestSides(triangles) {
+  //Input Validation
   if (!triangles) throw new Error("triangles is required");
-  // Your code here
+
+  // Taking the max number of array triangles 
+  const longNum = [];
+  triangles.forEach(triangle => longNum.push(Math.max(...triangle)));
+  return longNum;
 }
 
 module.exports = {
